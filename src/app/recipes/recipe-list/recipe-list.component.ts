a1fadc671c2c067c55caf9bf75f7751062a42f2c
Moshe Solomon <udeleng@gmail.com>
2017-05-20 22:21:37 -0700

    Hooked selection event on recipe to update recipe details component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,16 +8,23 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
 	recipes: Recipe[] = [
-		new Recipe('A Test Recipe', 'This is simply a test', 
+		new Recipe('A Test Recipe 1', 'This is simply a test 1', 
 			'https://upload.wikimedia.org/wikipedia/commons/8/84/Mutton_Biriyani_-_Rich_Maha%2C_Vermont_South_%28184564933%29.jpg'),
-		new Recipe('A Test Recipe', 'This is simply a test', 
+		new Recipe('A Test Recipe 2', 'This is simply a test 2', 
 			'https://upload.wikimedia.org/wikipedia/commons/8/84/Mutton_Biriyani_-_Rich_Maha%2C_Vermont_South_%28184564933%29.jpg')
 	];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
