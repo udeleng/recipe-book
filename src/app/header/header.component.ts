e4e2919bfcd2776d35d71cd3e1996492ad7a874a
Moshe Solomon <udeleng@gmail.com>
2017-08-03 22:29:23 -0700

    Added recipes and shopping-list routes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,16 +7,9 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  @Output()
-  featureSelected = new EventEmitter<string>();
-
   constructor() { }
 
   ngOnInit() {
-  }
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
   }
 
 }
