b87ed02167d8b2aa011dcf9ea563b4e74f9ba6cf
Moshe Solomon <udeleng@gmail.com>
2017-08-06 00:45:44 -0700

    Added recipes sub routes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -34,4 +34,8 @@ export class RecipeService {
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.shoppingListService.addIngredients(ingredients);
     }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
+    }
 }
